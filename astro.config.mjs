8aae0c3132891dce6728616533f270f2d5342b37
Iggy Spolski <ignacyjanuszek@gmail.com>
2025-01-04 21:01:40 +0100

    edit link

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://solarisproject.netlify.app/"
+    site: "https://github.com/Ignisko/ignisko.github.io"
 });
 
